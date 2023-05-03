a2629c7acacf751924e6c65f433182ae567cecb3
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-03 12:39:13 +0000

    [add]deviseの導入、deviseのcontroller作成、deviseのview作成、deviseのルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :admin,skip:[:registrations,:passwords],controllers:{
+    sessions:"admin/sessions"
+  }
+
+  devise_for :customers,skip:[:passwords],controllers:{
+    registrations:"public/registrations",
+    sessions:'public/sessions'
+  }
+
+  scope module: :public do
+    root to:'homes#top'
+    get 'about'=>'homes#about',as:'about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
