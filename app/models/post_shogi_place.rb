ba8601457ac3ef8503fbcc613eed1bd57f7ff05a
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-04 13:17:21 +0000

    [update]shogi_placeモデル

--- app/models/post_shogi_place.rb
+++ app/models/post_shogi_place.rb
@@ -6,12 +6,12 @@ class PostShogiPlace < ApplicationRecord
   belongs_to:prefecture
 
   has_one_attached:shogi_place_image
-  
+
   def get_shogi_place_image
-    unless shogi_placeimage.attached?
+    unless shogi_place_image.attached?
       file_path=Rails.root.join('app/assets/images/no_image.jpg')
-      customer_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
+      shogi_place_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
     end
-    customer_image.variant(resize_to_limit:[width,height]).processed
+    shogi_place_image.variant(resize_to_limit:[width,height]).processed
   end
 end
