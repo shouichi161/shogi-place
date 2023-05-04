2fe9e7ecc3393e439b925384f39d8577e884f744
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-04 13:16:05 +0000

    [add]active_storage：install,画像を使用するための記述、画像のサイズ変更gem,画像が存在しない場合の表示する画像についての記述、画像サイズの変換記述

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,17 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many:post_shogi_places,dependent:destroy
-  has_many:postcomments,dependent:destroy
-  has_many:favorites,dependent:destroy
+  has_many:postcomments,     dependent:destroy
+  has_many:favorites,        dependent:destroy
+
+  has_one_attached:customer_image
+
+  def get_customer_image
+    unless customer_image.attached?
+      file_path=Rails.root.join('app/assets/images/no_image.jpg')
+      customer_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
+    end
+    customer_image.variant(resize_to_limit:[width,height]).processed
+  end
+
 end
--- app/models/post_shogi_place.rb
+++ app/models/post_shogi_place.rb
@@ -4,4 +4,14 @@ class PostShogiPlace < ApplicationRecord
   has_many:activitys_tag_relations,dependent:destroy
   belongs_to:customer
   belongs_to:prefecture
+
+  has_one_attached:shogi_place_image
+  
+  def get_shogi_place_image
+    unless shogi_placeimage.attached?
+      file_path=Rails.root.join('app/assets/images/no_image.jpg')
+      customer_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
+    end
+    customer_image.variant(resize_to_limit:[width,height]).processed
+  end
 end
