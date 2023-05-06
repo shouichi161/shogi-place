77a59611ee78d12424f0ef4844aa6e139ed3bc8e
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-06 05:03:52 +0000

    [fix]customer,post_shogi_placeモデルの画像を入れるメソッドの記述修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,7 +12,7 @@ class Customer < ApplicationRecord
 
   enum gender:{male:0,woman:1}
 
-  def get_customer_image
+  def get_customer_image(width,height)
     unless customer_image.attached?
       file_path=Rails.root.join('app/assets/images/no_image.jpg')
       customer_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
