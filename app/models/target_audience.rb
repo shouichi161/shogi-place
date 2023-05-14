f30f26a18a350ec7966543afe607bb6d48ed6256
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-14 10:34:55 +0000

    [fix]target_audienceモデルの記述修正

--- app/models/target_audience.rb
+++ app/models/target_audience.rb
@@ -1,4 +1,4 @@
 class TargetAudience < ApplicationRecord
-  has_many:post_shogi_places,dependent: :destroy,foreign_key: 'tag_id'
+  has_many:target_audiences_tag_relations,dependent: :destroy
   has_many:post_shogi_places,through: :target_audiences_tag_relations
 end
