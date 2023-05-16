b6972f14afd49320ef19da3bccf71aa7d5d33fe8
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-16 12:48:49 +0000

    [add]コメントにバリデーション追加、投稿詳細画面にコメントを空欄で投稿時エラーメッセージの表示

--- app/controllers/public/postcomments_controller.rb
+++ app/controllers/public/postcomments_controller.rb
@@ -1,10 +1,16 @@
 class Public::PostcommentsController < ApplicationController
   def create
-    post_shogi_place=PostShogiPlace.find(params[:post_shogi_place_id])
-    comment=current_customer.postcomments.new(postcomment_params)
-    comment.post_shogi_place_id=post_shogi_place.id
-    comment.save
-    redirect_to post_shogi_place_path(post_shogi_place)
+    @post_shogi_place=PostShogiPlace.find(params[:post_shogi_place_id])
+    @postcomment=current_customer.postcomments.new(postcomment_params)
+    @postcomment.post_shogi_place_id=@post_shogi_place.id
+    if @postcomment.save
+       flash[:notice]="コメントを投稿しました"
+       redirect_to post_shogi_place_path(@post_shogi_place)
+    else
+       @tags=@post_shogi_place.tags
+       @postcomments=@post_shogi_place.postcomments.all
+       render:'public/post_shogi_places/show'
+    end
   end
 
   def destroy
--- app/models/postcomment.rb
+++ app/models/postcomment.rb
@@ -1,4 +1,6 @@
 class Postcomment < ApplicationRecord
   belongs_to:customer
   belongs_to:post_shogi_place
+
+  validates:comment,presence:true
 end
