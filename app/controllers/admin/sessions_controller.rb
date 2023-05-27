fd5787ed969a2017da065d7d00e1d283c4376012
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-27 09:40:24 +0000

    [add]管理者ログイン後、ログアウト後の移動先指定

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
 
+  def after_sign_out_path_for(resource)
+    admin_session_path
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
