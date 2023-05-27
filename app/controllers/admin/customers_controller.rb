36e27c7ab580b42886d48cde633db24504525c59
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-27 06:18:12 +0000

    [add]会員情報編集画面、会員情報更新機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,8 +10,22 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
 
   def update
+    @customer=Customer.find(params[:id])
+    if @customer.update(customer_params)
+       flash[:notice]="会員情報を更新しました。"
+       redirect_to admin_customer_path(@customer.id)
+    else
+       render:edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name,:email,:date_of_birth,:gender,:chess_ability,:profile,:membership_status)
   end
 end
