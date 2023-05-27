eb80f697785471e35670b0b5b11c276e46af0f8e
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-27 09:20:10 +0000

    [add]会員詳細画面に投稿者、投稿内容、コメント情報を追加

--- app/controllers/admin/post_shogi_places_controller.rb
+++ app/controllers/admin/post_shogi_places_controller.rb
@@ -1,8 +1,13 @@
 class Admin::PostShogiPlacesController < ApplicationController
   def show
     @post_shogi_place=PostShogiPlace.find(params[:id])
+    @target_audiences=@post_shogi_place.target_audiences
+    @postcomments=@post_shogi_place.postcomments.page(params[:page])
   end
 
   def destroy
+    post_shogi_place=PostShogiPlace.find(params[:id])
+    post_shogi_place.destroy
+    redirect_to admin_path
   end
 end
--- app/controllers/admin/postcomments_controller.rb
+++ app/controllers/admin/postcomments_controller.rb
@@ -2,6 +2,7 @@ class Admin::PostcommentsController < ApplicationController
   def destroy
     postcomment=Postcomment.find(params[:id])
     postcomment.destroy
+    flash[:notice]="コメントを削除しました。"
     redirect_to admin_post_shogi_place_path(params[:post_shogi_place_id])
   end
 end
