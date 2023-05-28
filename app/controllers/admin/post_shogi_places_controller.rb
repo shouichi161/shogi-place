276e4d1f94a9a75aecf09a93c73c853cd20695d5
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-05-28 10:26:39 +0000

    [update]投稿詳細ページにタグ一覧追加、表示変更

--- app/controllers/admin/post_shogi_places_controller.rb
+++ app/controllers/admin/post_shogi_places_controller.rb
@@ -2,6 +2,7 @@ class Admin::PostShogiPlacesController < ApplicationController
   before_action:authenticate_admin!
   def show
     @post_shogi_place=PostShogiPlace.find(params[:id])
+    @tags=@post_shogi_place.tags
     @target_audiences=@post_shogi_place.target_audiences
     @postcomments=@post_shogi_place.postcomments.page(params[:page])
   end
