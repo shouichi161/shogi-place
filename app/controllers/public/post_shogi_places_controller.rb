da983b4e656a2208d27333d62ef080455d28de49
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-06-11 13:11:28 +0000

    [update]会員側投稿詳細画面の記述変更

--- app/controllers/public/post_shogi_places_controller.rb
+++ app/controllers/public/post_shogi_places_controller.rb
@@ -27,6 +27,7 @@ class Public::PostShogiPlacesController < ApplicationController
   def show
     @post_shogi_place=PostShogiPlace.find(params[:id])
     @tags=@post_shogi_place.tags
+    @target_audiences=@post_shogi_place.target_audiences
     @postcomments=@post_shogi_place.postcomments.page(params[:page])
     @postcomment=Postcomment.new
   end
