8e7a870a0563f3952efc413c983639edd7480a6a
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-06-30 12:58:38 +0000

    [update]save_tagメソッドの記述を更新（古いタグがないときはsent_tagsをそのまま保存する)

--- app/models/post_shogi_place.rb
+++ app/models/post_shogi_place.rb
@@ -22,21 +22,29 @@ class PostShogiPlace < ApplicationRecord
 
   def save_tag(sent_tags)
   # タグが存在していれば、タグの名前を配列として全て取得
-    current_tags=self.tags.pluck(:tag_name) unless self.tags.nil?
+    current_tags=self.tags.pluck(:tag_name)
+    unless self.tags.nil?
   # 現在取得したタグから送られてきたタグを除いてoldtagとする
-    old_tags = current_tags - sent_tags
+      old_tags = current_tags - sent_tags
   # 送信されてきたタグから現在存在するタグを除いたタグをnewとする
-    new_tags = sent_tags - current_tags
+      new_tags = sent_tags - current_tags
 
     #古いタグを消す
-    old_tags.each do |old|
+      old_tags.each do |old|
       self.tags.delete Tag.find_by(name: old)
     end
 
     # 新しいタグを保存
-    new_tags.each do |new|
+      new_tags.each do |new|
+      new_tagging=Tag.find_or_create_by(name: new)
+        self.tags << new_tagging
+      end
+    else
+    # 新しいタグを保存
+      sent_tags.each do |new|
       new_tagging=Tag.find_or_create_by(name: new)
       self.tags << new_tagging
+      end
     end
   end
 
